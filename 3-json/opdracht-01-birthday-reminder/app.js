b56dbb59f5cd64e13f3e07d86ae3006cab429bb0
Juliany Rosenblad <9024932@student.zadkine.nl>
2025-09-30 15:25:57 +0200

    Opdracht - Birthday reminder

--- 3-json/opdracht-01-birthday-reminder/app.js
+++ 3-json/opdracht-01-birthday-reminder/app.js
@@ -1,1 +1,21 @@
- 
+const buttonClear = document.querySelector(".clear-button");
+const birthdayContainer = document.querySelector('.birthday');
+const amountPeople = document.querySelector("AmountofPeople");
+
+
+fetch("./birthday.json")
+.then((response) => response.json())
+.then((birthday) => {
+    buttonClear.addEventListener('click', function(){
+        birthdayContainer.innerHTML = "";
+        amountPeople.innerHTML = `<h2>0 verjaardagen vandaag</h2>`;
+    });
+
+    for (let mensen of birthday) {
+        birthdayContainer.innerHTML += `
+            <h2>naam: ${mensen.name}</h2>
+            <p>leeftijd: ${mensen.age}</p>
+            <img class="image" src="${mensen.image}">
+        `;
+    }
+});
